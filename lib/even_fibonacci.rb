6ca7b05006e37f2c92a739513b61402928a9c53b
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 01:46:55 +0000

    Automatically backed up by Learn

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,11 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  array = [1, 2]
+  i = 1
+   while  i < limit 
+    next_number = array[i] + array[i-1]
+    array << next_number
+    i += 1 
+  end
+  array
+end
