01b393e7017db66545b0333b96aeac85c1679772
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 01:55:57 +0000

    Automatically backed up by Learn

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -3,7 +3,7 @@ def even_fibonacci_sum(limit)
   array = [1, 2]
   limit_reached = false
   i = 1
-  while  next_number < limit 
+  while  !limit_reached 
     next_number = array[i] + array[i-1]
     if next_number < limit
       array << next_number 
