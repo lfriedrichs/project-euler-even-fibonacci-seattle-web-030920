fba5ebea81a7ec6a1056668fa92b29ee34674780
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-02 01:53:14 +0000

    Automatically backed up by Learn

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -4,7 +4,7 @@ def even_fibonacci_sum(limit)
   i = 1
    while  array[-1] < limit 
     next_number = array[i] + array[i-1]
-    array << next_number
+    array << next_number if next_number < limit
     i += 1 
   end
   array.select {|number| number % 2 == 0}
